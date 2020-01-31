c9396a0dbdfd63553a00bcb6506c2c14db1eba09
erik aspen <erikaa17@uia.no>
2020-01-31 16:05:28 +0100

    Rep fix

---
+++ JFX-MultiScreen/ScreensFramework/src/module-info.java
@@ -0,0 +1,13 @@
+module ScreensFramework{
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+
+    opens org.openjfx to javafx.fxml;
+    exports org.openjfx;
+}
